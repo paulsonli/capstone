539704e0fcccc02c69732c708b2e156c96605c00
Paulson Li <paulsonli99@gmail.com>
2017-04-29 21:01:43 +0000

    wall collision

--- javascript.js
+++ javascript.js
@@ -1,17 +1,40 @@
 /*global $*/
 $(document).ready(function (){
-    function checkCollision (){
-        
-        var paddle = $("#paddle").offset().top ; 
-        
+    $("#paddle").css("left", "50%");
+    
+    
+    
+
+
+    
+});    
+
+//wall collision   
+function wallBlock() {
+    var paddleLeft = $("#paddle").offset().left;
+    var paddleRight = paddleLeft + $("#paddle").width();
+    var wallLeft = $("#container").offset().left;
+    var wallRight = wallLeft + $("#container").width();
+    if (paddleLeft <= wallLeft || paddleRight >= wallRight) {
+        alert ("hi");
     }
+    console.log(wallRight);
+}    
     
-    
-    
-    if(){
-        
+
+//paddle movement    
+$("body").keydown(function(event) {
+
+    if (event.which === 37) {
+        $("#paddle").css("left", $("#paddle").offset().left - 9);
+    } else if (event.which === 39) {
+        $("#paddle").css("left", $("#paddle").offset().left + 9);
     } else {
-        
+        return;
     }
+    wallBlock();
+    console.log(event);
     
 });
+
+
