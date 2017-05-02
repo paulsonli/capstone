591b9a947fa41a3b8924db399cf67348da7e2e39
Paulson Li <paulsonli99@gmail.com>
2017-05-02 21:23:47 +0000

    missile tilted

--- javascript.js
+++ javascript.js
@@ -1,7 +1,27 @@
 /*global $*/
 $(document).ready(function (){
+    var width = $("#container").width();
+    var RNGspawn = window.innerWidth * 0.25 + Math.floor((Math.random() * width) + 1);
+    $("#missile").css("left", RNGspawn);
     $("#paddle").css("left", "50%");
-    
+    //spawn bombs
+    var interval = setInterval(function() {
+        var picTop = $("#missile").offset().top;
+        var picBottom = picTop + $("#missile").height();
+        var paddleTop = $("#paddle").offset().top;
+        if (picBottom >= paddleTop) {
+            $("#missile").hide();
+            clearInterval(interval);
+        }
+        console.log("picTop:" + picTop);
+        console.log("picBottom:"  + picBottom);
+        console.log("paddleTop " + paddleTop);
+        console.log("width " + width);
+        console.log("spawn " + RNGspawn);
+        $("#missile").css("top", $("#missile").offset().top + 10);
+    // if missile touches container bottom, delete it delete.pic or something like that
+
+    },50);
     
     
 
@@ -16,7 +36,7 @@ function wallBlock() {
     var wallLeft = $("#container").offset().left;
     var wallRight = wallLeft + $("#container").width();
     if (paddleLeft <= wallLeft || paddleRight >= wallRight) {
-        alert ("hi");
+        alert ("hi"); //change
     }
     console.log(wallRight);
 }    
